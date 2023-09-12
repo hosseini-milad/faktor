7a67a4b18a686c4a994ddaf52b1478d3e7c17db5
hosseini.milad1400 <hosseini.milad1400@gmail.com>
2023-09-12 19:04:33 +0330

    api

--- router/productApi.js
+++ router/productApi.js
@@ -514,7 +514,7 @@ router.post('/update-faktor',jsonParser, async (req,res)=>{
         }
         const recieptQuery = await RecieptFunc(req.body.receiptInfo,addFaktorResult[0],faktorNo)
         const recieptResult = await sepidarPOST(recieptQuery,"/api/Receipts/BasedOnInvoice")
-        const SepidarFaktor = await SepidarFunc(faktorDetail)
+        //const SepidarFaktor = await SepidarFunc(faktorDetail)
         if(recieptResult.Message){
             res.json({error:recieptResult.Message,status:"reciept"})
                 return
