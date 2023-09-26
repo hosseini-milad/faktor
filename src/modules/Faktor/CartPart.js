dc8100cdc31dcbac399f58b4bf33713481591d7e
hosseini.milad1400 <hosseini.milad1400@gmail.com>
2023-09-26 15:59:11 +0330

    duplicate changes

--- src/modules/Faktor/CartPart.js
+++ src/modules/Faktor/CartPart.js
@@ -8,7 +8,6 @@ import CartRegAccordion from "../Cart/CartRegAccordion"
 function CartPart(props){
     console.log(props.faktorList)
     return(<>
-<<<<<<< HEAD
             {(props.faktorList&&props.faktorList.cart&&props.faktorList.cart.length)?
                     props.faktorList.cart.map((cart,i)=>(
                         <div className="accordions" key={i}>
@@ -17,41 +16,6 @@ function CartPart(props){
                             </div>
                         )):<></>}
                 <table><tbody>
-=======
-        <table style={{overflow: "auto"}}>
-            <thead>
-                <tr>
-                    <th width="3%">ردیف</th>
-                    <th width="30%">شرح</th>
-                    <th width="10%">تعداد</th>
-                    <th width="10%">قیمت</th>
-                    <th width="10%">توضیحات</th>
-                    <th width="10%">عملیات</th>
-                </tr>
-            </thead>
-            <tbody>
-                {(props.faktorList&&props.faktorList.totalCount)?
-                    props.faktorList.cart.cartItems.map((faktor,i)=>(
-                <tr key={i} style={{backgroundColor:"#EEEFFC"}}>
-                    <td>{i+1}</td>
-                    <td className="tdHolder">
-                        <strong>{faktor.title}</strong>
-                        <small>{faktor.sku}</small></td>
-                    <td style={{textAlign:"center"}}>{/*<div className="form-fiin form-field-fiin" style={{marginBottom: "0"}}>
-                        <input type="text" name="count" id="count" 
-                            onChange={(e)=>setCount(faktor.id,e.target.value)}
-                            className="formInputSimple"
-                            value={faktor.count||''}
-                    placeholder="تعداد"/>
-                        </div>*/}{faktor.count}</td>
-                    <td style={{textAlign:"center"}}>{normalPrice(faktor.price)}</td>
-                    <td>{faktor.description}</td>
-                    <td>{<FaktorReturn itemId={faktor} setFaktorList={props.setFaktorList}
-                        user={props.user} token={props.token} itemCount={faktor.count}/>}</td>
-                </tr>
-                )):
-                <tr><td colSpan={2}>سبد خرید خالی است</td></tr>}
->>>>>>> ab4e8d03e89cb21d0e37e488a24b563c177bcfc5
                 {props.faktorList&&props.faktorList.totalCount?
                   <tr style={{backgroundColor:"#666A70",color:"#fff"}}>
                     <td>#</td>
