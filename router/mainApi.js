9dd5a000867c81b7d3391ab4dbb601586f299b00
milad <milad.cia@gmail.com>
2024-02-06 19:32:05 +0330

    description

--- router/mainApi.js
+++ router/mainApi.js
@@ -214,7 +214,7 @@ router.get('/sepidar-quantity', async (req,res)=>{
                 
                 var intBox =0
                 try{intBox=(parseInt(Math.round(perBox)))} catch{}
-                if(perBox!==1)await products.updateOne({
+                if(perBox!==1&&intBox!==0)await products.updateOne({
                     ItemID:sepidarQuantityResult[i].ItemRef,
                 },{$set:{perBox:intBox}})
             }
